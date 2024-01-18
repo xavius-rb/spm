98568d5f2951e83ca2f59f9f1cc8dc42a3ea8883
Emerson Xavier <emerson.xavier@readytech.io>
2024-01-18 17:25:32 +1100

    refactor

--- app/services/events_report.rb
+++ app/services/events_report.rb
@@ -1,26 +1,41 @@
 class EventsReport
   CUTOFF_EVENT = "PURCHASE"
+  SUCCESSFUL_JOURNEY_REQUIREMENTS = %W(PURCHASE ADD_TO_CART VIEW_PRODUCT)
+
+  attr_reader :events
 
   def initialize(events)
-    puts events.inspect
     @events = events
+    @journeys = Hash.new {|h,k| h[k] = [] }
   end
 
   def generate
-    journeys = {}
     journey_id = 0
-    @events.each do |e|
-      journeys[journey_id.to_s] ||= []
-      journeys[journey_id.to_s] << e if e.downcase.in?(["purchase", "add_to_cart", "view_product"])
-      if e == CUTOFF_EVENT && successfull?(journeys[journey_id.to_s])
+    tmp_journey = []
+    events.each do |event|
+      if event.in?(SUCCESSFUL_JOURNEY_REQUIREMENTS)
+        tmp_journey << event
+      else
+        # successful journey interrupted
+        tmp_journey.clear
+      end
+
+      if event == CUTOFF_EVENT && successful_journey?(tmp_journey)
+        add_journey(journey_id, tmp_journey.dup)
         journey_id =+ 1
+        tmp_journey.clear
       end
     end
-    journeys
+
+    @journeys
   end
 
-  def successfull?(journeys)
-    (journeys & ["purchase", "add_to_cart", "view_product"].map(&:upcase)).size == 3
+  def successful_journey?(journey)
+    (journey & SUCCESSFUL_JOURNEY_REQUIREMENTS).size == SUCCESSFUL_JOURNEY_REQUIREMENTS.size
   end
 
+  private
+  def add_journey(key, journey)
+    @journeys[key.to_s] = journey
+  end
 end
--- spec/services/events_report_spec.rb
+++ spec/services/events_report_spec.rb
@@ -2,14 +2,26 @@ require 'rails_helper'
 
 RSpec.describe EventsReport do
   describe '#generate' do
-    events = %w(PURCHASE VIEW_PRODUCT ADD_TO_CART PURCHASE REMOVE_FROM_CART ADD_TO_CART VIEW_PRODUCT PURCHASE ADD_TO_CART PURCHASE)
-    report = EventsReport.new(events)
+    let(:result) { described_class.new(events).generate }
+    let(:events) do
+      %w(PURCHASE VIEW_PRODUCT ADD_TO_CART PURCHASE REMOVE_FROM_CART ADD_TO_CART VIEW_PRODUCT PURCHASE ADD_TO_CART PURCHASE)
+    end
 
     it do
-      result = report.generate
       expect(result.size).to eq 2
       expect(result["0"]).to eq(["PURCHASE", "VIEW_PRODUCT", "ADD_TO_CART", "PURCHASE"])
-      expect(result["1"]).to eq(%w(ADD_TO_CART VIEW_PRODUCT PURCHASE ADD_TO_CART PURCHASE))
+      expect(result["1"]).to eq(%w(ADD_TO_CART VIEW_PRODUCT PURCHASE))
+    end
+
+    context "interrupted journey" do
+      let(:events) do
+        %w(PURCHASE VIEW_PRODUCT ADD_TO_CART LOGOUT PURCHASE REMOVE_FROM_CART ADD_TO_CART VIEW_PRODUCT PURCHASE ADD_TO_CART PURCHASE)
+      end
+
+      it do
+        expect(result.size).to eq 1
+        expect(result["0"]).to eq(%w(ADD_TO_CART VIEW_PRODUCT PURCHASE))
+      end
     end
   end
 end
